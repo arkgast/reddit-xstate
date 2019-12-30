000c0fb8bf047ca93befe37adfa84bb7788cdb74
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-12-30 11:25:09 -0400

    feat: add reddit machine

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log('xstate rocks!')
+const { Machine, assign } = require('xstate')
+const axios = require('axios')
+
+const invokeFetchSubreddit = context => {
+  const { subreddit } = context
+  return axios
+    .get(`https://www.reddit/r/${subreddit}.json`)
+    .then(json => json.data.children.map(child => child.data))
+}
+
+const redditMachine = Machine({
+  id: 'reddit',
+  initial: 'idle',
+  context: {
+    subreddit: null,
+    posts: null
+  },
+  states: {
+    idle: {},
+    selected: {
+      initial: 'loading',
+      states: {
+        loading: {
+          invoke: {
+            id: 'fetch-subreddit',
+            src: invokeFetchSubreddit
+          },
+          onDone: {
+            target: 'loaded',
+            actions: assign({ posts: (context, event) => event.data })
+          },
+          onError: 'failed'
+        },
+        loaded: {},
+        failed: {}
+      }
+    }
+  },
+  on: {
+    SELECT: '.selected',
+    actions: assign({
+      subreddit: (context, event) => event.name
+    })
+  }
+})
+
+module.exports = {
+  redditMachine
+}
