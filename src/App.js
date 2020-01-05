1e58fe078e313c2e810046d7e3365afc51ab3798
Arnold Gandarillas Castillo <arkgast@gmail.com>
2020-01-04 23:23:42 -0400

    feat: add ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useMachine } from '@xstate/react'
+import { redditMachine } from './redditMachine'
+
+const subreddits = ['frontend', 'reactjs', 'vuejs']
 
 function App () {
-  return <h1>react-xstate</h1>
+  const [current, send] = useMachine(redditMachine)
+  const { subreddit, posts } = current.context
+
+  const handleSelect = event => {
+    const { value } = event.target
+    send('SELECT', { name: value })
+  }
+
+  return (
+    <main>
+      <header>
+        <select onChange={handleSelect}>
+          {subreddits.map(subreddit => (
+            <option key={subreddit}>{subreddit}</option>
+          ))}
+        </select>
+      </header>
+      <section>
+        <h1>{current.matches('idle') ? 'Select a subreddit' : subreddit}</h1>
+        {current.matches({ selected: 'loading' }) && <div>Loading ...</div>}
+        {current.matches({ selected: 'loaded' }) && (
+          <ul>
+            {posts.map(post => (
+              <li key={post.title}>{post.title}</li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </main>
+  )
 }
 
 export default App
